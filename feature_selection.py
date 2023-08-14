6d5d9f3145d7754a2180194a77ef466093d35896
Hyeonsik <38666464+Hyeonsik@users.noreply.github.com>
2023-08-14 21:23:46 +0900

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -11,31 +11,43 @@ SEED = 98
 np.random.seed(SEED)
 
 # Define variables
-INPUT_VARS = ['age_cal','sex', 'weight','height', 'cuffed']
+INPUT_VARS = ['age','sex', 'weight','height']
 OUTPUT_VAR = 'airway_tube_size'
 
 # Load data
-df = pickle.load(open('data_size', 'rb'))
-df = shuffle(df, random_state=SEED)
+df = pd.read_csv('dataset/df_size_2308.csv', index_col = 0)
 
-x = df.loc[:, INPUT_VARS].values.astype(float)
-y = df[[OUTPUT_VAR]].values.flatten().astype(float)
+# Sort the data in ascending order based on the 'opdate' column (most recent first)
+df.sort_values(by=['opdate'], ascending=True, inplace=True)
 
-# Split data into train and test sets
-nsamp = len(y)
+# Extract features (x) and target (y) values from the dataFrame
+x = df[['age_cal', 'sex', 'weight', 'height', 'cuffed']].astype(float).values
+y = df['airway_tube_size'].astype(float).values
+c = df['opid'].values
+
+# Separate the data for cuffed and uncuffed endotracheal tubes
+## Cuffed endotrahceal tube
+x_cuffed = x[x[:, 4] == 1][:, :4]
+y_cuffed = y[x[:, 4] == 1]
+
+# Determine the size of the test and train sets for cuffed data
+nsamp = len(y_cuffed)
 ntest = int(nsamp * 0.2)
 ntrain = nsamp - ntest
-x_test = x[-ntest:, :]
-y_test = y[-ntest:]
-y_test_old = y_old[-ntest:]
-x_train = x[:ntrain, :]
-y_train = y[:ntrain]
 
-# Multiple imputation for missing value
-imp = IterativeImputer().fit(x_train)
-x_train_imputed = imp.transform(x_train)
-x_test_imputed = imp.transform(x_test)
-X = pd.DataFrame(x_train_imputed, columns=INPUT_VARS)
+# Split the cuffed data into training and test sets
+x_cuff_test = x_cuffed[-ntest:, :]
+y_cuff_test = y_cuffed[-ntest:]
+x_cuff_train = x_cuffed[:ntrain, :]
+y_cuff_train = y_cuffed[:ntrain]
+
+# Impute missing values in the cuffed data using the multiple imputation method
+imp = IterativeImputer().fit(x_cuff_train)
+x_cuff_train = imp.transform(x_cuff_train)
+x_cuff_test = imp.transform(x_cuff_test)
+
+# Create a DataFrame with the training features (X) from the cuffed data
+X = pd.DataFrame(x_cuff_train, columns=INPUT_VARS)
 
 # Set up the model
 xgbr = xgb.XGBRegressor(random_state=SEED, tree_method='gpu_hist')
@@ -49,7 +61,7 @@ Feature_Selector = BorutaShap(model=xgbr,
 
 # Fit the feature selector
 Feature_Selector.fit(X=X, 
-                     y=y_train, 
+                     y=y_cuff_train, 
                      n_trials=100, 
                      sample=False, 
                      train_or_test = 'train', 
@@ -58,4 +70,55 @@ Feature_Selector.fit(X=X,
                      random_state=SEED)
 
 # Plotting the BorutaSHAP 
+print('Boxplot chart illustrating BorutaSHAP result for cuffed endotracheal tubes')
+Feature_Selector.plot(X_size=15, which_features='all')
+
+
+
+## Uncuffed endotracheal tube
+x_uncuffed = x[x[:, 4] == 0][:, :4]
+y_uncuffed = y[x[:, 4] == 0]
+
+# Determine the size of the test and train sets for uncuffed data
+nsamp = len(y_uncuffed)
+ntest = int(nsamp * 0.2)
+ntrain = nsamp - ntest
+
+# Split the uncuffed data into training and test sets
+x_uncuff_test = x_uncuffed[-ntest:, :]
+y_uncuff_test = y_uncuffed[-ntest:]
+x_uncuff_train = x_uncuffed[:ntrain, :]
+y_uncuff_train = y_uncuffed[:ntrain]
+
+# Impute missing values in the uncuffed data using the multiple imputation method
+imp = IterativeImputer().fit(x_uncuff_train)
+x_uncuff_train = imp.transform(x_uncuff_train)
+x_uncuff_test = imp.transform(x_uncuff_test)
+
+
+# Create a DataFrame with the training features (X) from the uncuffed data
+X = pd.DataFrame(x_uncuff_train, columns=INPUT_VARS)
+
+# Set up the model
+xgbr = xgb.XGBRegressor(random_state=SEED, tree_method='gpu_hist')
+
+# Set up the feature selector
+Feature_Selector = BorutaShap(model=xgbr, 
+                              importance_measure='shap', 
+                              classification=False, 
+                              percentile=100, 
+                              pvalue=0.05)
+
+# Fit the feature selector
+Feature_Selector.fit(X=X, 
+                     y=y_uncuff_train, 
+                     n_trials=100, 
+                     sample=False, 
+                     train_or_test = 'train', 
+                     normalize=True, 
+                     verbose=False, 
+                     random_state=SEED)
+
+# Plotting the BorutaSHAP 
+print('Boxplot chart illustrating BorutaSHAP result for uncuffed endotracheal tubes')
 Feature_Selector.plot(X_size=15, which_features='all')
